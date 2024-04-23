4d3730622bd69297f03128d2b641719bdc5374e7
Julia Darwish <darwishjulia4@gmail.com>
2024-04-22 20:42:01 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterPage from "./components/RegisterPage";
+import WelcomePage from "./components/WelcomePage";
+import CocktailPage from "./components/CocktailPage";
+// import ShopPage from "./components/ShopPage";
 
 function App() {
+  const [username, setUsername] = useState("");
+
   return (
     <div className="App">
-   
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<RegisterPage setUsername={setUsername} />}
+          />
+          <Route
+            path="/welcome-page"
+            element={<WelcomePage username={username} />}
+          />
+          {/* <Route path="/shop-page" element={<ShopPage username={username} />} /> */}
+          <Route
+            path="/cocktail-page"
+            element={<CocktailPage username={username} />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
